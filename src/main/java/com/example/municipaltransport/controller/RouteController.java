697f27caeedb2e1d9761a78b7e1572aba8853f7b
nilsson46 <nilssonsimon46@gmail.com>
2023-02-23 16:55:26 +0100

    Added a get mapping in the controller class

--- src/main/java/com/example/municipaltransport/controller/RouteController.java
+++ src/main/java/com/example/municipaltransport/controller/RouteController.java
@@ -1,11 +1,28 @@
 package com.example.municipaltransport.controller;
 
+import com.example.municipaltransport.model.Route;
+import com.example.municipaltransport.service.RouteService;
 import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
 public class RouteController {
 
     @Autowired
+    private RouteService routeService;
+
+    private List<Route> routeList;
+
+    @GetMapping("channels")
+    public ResponseEntity<List<Route>> getRoutes(){
+        List<Route> routeList = routeService.getAll();
+        return ResponseEntity.ok(routeList);
+    }
+    
 }
