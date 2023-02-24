e2c7b21ae08f9cfeea4a5607c8605569b4fcfbec
nilsson46 <nilssonsimon46@gmail.com>
2023-02-24 12:02:15 +0100

    Added get and post mapping

--- src/main/java/com/example/municipaltransport/model/Route.java
+++ src/main/java/com/example/municipaltransport/model/Route.java
@@ -3,14 +3,12 @@ package com.example.municipaltransport.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter@Setter
+@ToString
 @Entity
 public class Route {
 
@@ -19,9 +17,9 @@ public class Route {
     private long id;
     private String startLocation;
     private String endLocation;
-    private int departure;
-    private int arrival;
+    private String departure;
+    private String arrival;
     private int travelTime;
     private int changes;
-    private boolean favorite; 
+    private boolean isFavorite;
 }
--- src/main/java/com/example/municipaltransport/service/RouteService.java
+++ src/main/java/com/example/municipaltransport/service/RouteService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RouteService {
@@ -15,8 +16,24 @@ public class RouteService {
     public List<Route> getAll(){
         return routeRepository.findAll();
     }
-    public Route findByStartLocation(String startLocation){
+
+    public List<Route> findByStartLocation(String startLocation){
         return routeRepository.findRouteByStartLocation(startLocation);
     }
 
+    public Route save(Route route) {
+        return routeRepository.save(route);
+    }
+
+    public List<Route> findByEndLocation(String endLocation) {
+        return  routeRepository.findRouteByEndLocation(endLocation);
+    }
+
+    public Optional<Route> findById(Long routeId) {
+        return routeRepository.findById(routeId);
+
+    }
+
+    public List<Route> findByIsFavorite() {
+    }
 }
